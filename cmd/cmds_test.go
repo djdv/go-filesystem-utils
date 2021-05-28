4b3f65e0c1d7041c528aefd08fe8b73c9738f237
Dominic Della Valle <ddvpublic@gmail.com>
2021-05-28 11:14:25 -0400

    refactor: generic argument test checks

--- cmd/cmds_test.go
+++ cmd/cmds_test.go
@@ -19,215 +19,246 @@ var root = &cmds.Command{
 	},
 }
 
+func genericGetArgument(method interface{},
+	goArguments []reflect.Value) (got reflect.Value, provided bool, err error) {
+	returnVector := reflect.ValueOf(method).Call(goArguments)
+	got = returnVector[0]
+	provided = returnVector[1].Bool()
+	maybeErr := returnVector[2].Interface()
+	if maybeErr != nil {
+		var isErr bool
+		err, isErr = maybeErr.(error)
+		if !isErr {
+			err = fmt.Errorf("return value is wrong type:\n\twanted:%T\n\tgot:%t",
+				err, maybeErr)
+		}
+	}
+	return
+}
+
+type genericParameterArgumentPair struct {
+	// The parameter's keys
+	fscmds.CmdsParameterSet
+	// And the value for its methods.
+	arguments []reflect.Value
+}
+
 func TestRequestsStandard(t *testing.T) {
 	var (
-		ctx, cancel  = context.WithCancel(context.Background())
-		request, err = cmds.NewRequest(ctx, nil, nil, nil, nil, root)
-		callMethod   = func(method interface{},
-			goArguments []reflect.Value) (got reflect.Value, provided bool, err error) {
-			returnVector := reflect.ValueOf(method).Call(goArguments)
-			got = returnVector[0]
-			provided = returnVector[1].Bool()
-			maybeErr := returnVector[2].Interface()
-			if maybeErr != nil {
-				var isErr bool
-				if err, isErr = maybeErr.(error); !isErr {
-					err = fmt.Errorf("return value is wrong type:\n\twanted:%T\n\tgot:%t",
-						err, maybeErr)
-				}
-			}
-			return
-		}
-		checkNotProvided = func(t *testing.T, typeName string, provided bool, got reflect.Value) {
-			t.Helper()
-			if provided {
-				t.Fatalf("%s returned for bad request:\n\tProvided? %t\n\tValue: %#v",
-					typeName, provided, got)
-			}
-		}
-		checkProvided = func(t *testing.T, typeName string, provided bool) {
-			t.Helper()
-			if !provided {
-				t.Fatalf("%s option was provided but not detected in request", typeName)
-			}
-		}
-		checkSame = func(t *testing.T, typeName string, got reflect.Value, want interface{}) {
-			t.Helper()
-			if got.Interface().(interface{}) != want {
-				t.Logf("%T != %T\n", got, want)
-				t.Fatalf("%s option provided did not match input:\n\twant:%s\n\tgot:%s",
-					typeName,
-					want, got)
-			}
-		}
+		ctx, cancel           = context.WithCancel(context.Background())
+		request, requestError = cmds.NewRequest(ctx, nil, nil, nil, nil, root)
 	)
 	defer cancel()
+	if requestError != nil {
+		t.Fatal(requestError)
+	}
+
+	var (
+		expectedParameters = fscmds.CmdsParameterSet{
+			Name:        "test-argument",
+			Environment: "TEST_ENV",
+		}
+		expectedSet = genericParameterArgumentPair{
+			CmdsParameterSet: expectedParameters,
+			arguments: []reflect.Value{
+				reflect.ValueOf(request),
+				reflect.ValueOf(expectedParameters),
+			},
+		}
+		unexpectedParameters = fscmds.CmdsParameterSet{
+			Name:        "bad-argument",
+			Environment: "BAD_ENV",
+		}
+		unexpectedSet = genericParameterArgumentPair{
+			CmdsParameterSet: unexpectedParameters,
+			arguments: []reflect.Value{
+				reflect.ValueOf(request),
+				reflect.ValueOf(unexpectedParameters),
+			},
+		}
+	)
+	t.Run("Expected input", func(t *testing.T) { expectedArguments(t, request, expectedSet) })
+	t.Run("Erroneous input", func(t *testing.T) { erroneousArguments(t, request, unexpectedSet) })
+}
+
+func expectedArguments(t *testing.T, request *cmds.Request, set genericParameterArgumentPair) {
+	t.Helper()
+	for _, test := range []struct {
+		typeName  string
+		value     interface{}
+		getMethod interface{}
+	}{
+		{
+			"string",
+			"test string value",
+			fscmds.GetStringArgument,
+		},
+		{
+			"bool",
+			true,
+			fscmds.GetBoolArgument,
+		},
+	} {
+		var (
+			typeName = test.typeName
+			getOpt   = test.getMethod
+			optValue = test.value
+		)
+		t.Run(typeName, func(t *testing.T) {
+			t.Run("Options", func(t *testing.T) {
+				got, provided := genericCheckArgument(t,
+					getOpt, set.arguments)
+				genericArgWasNotProvided(t,
+					typeName, provided, got)
+
+				request.SetOption(set.Name, optValue)
+				got, provided = genericCheckArgument(t,
+					getOpt, set.arguments)
+				delete(request.Options, set.Name)
+
+				genericArgWasProvided(t,
+					typeName, provided)
+				genericArgIsSame(t,
+					typeName, got, optValue)
+			})
+			t.Run("Environment", func(t *testing.T) {
+				got, provided := genericCheckArgument(t,
+					getOpt, set.arguments)
+				genericArgWasNotProvided(t,
+					typeName, provided, got)
+
+				osErr := os.Setenv(set.Environment, fmt.Sprintf("%v", optValue))
+				if osErr != nil {
+					t.Fatalf("failed to set environment %q: %s",
+						set.Environment, osErr)
+				}
+				got, provided = genericCheckArgument(t,
+					getOpt, set.arguments)
+				if osErr := os.Unsetenv(set.Environment); osErr != nil {
+					t.Fatalf("failed to unset environment %q: %s",
+						set.Environment, osErr)
+				}
+
+				genericArgWasProvided(t,
+					typeName, provided)
+				genericArgIsSame(t,
+					typeName, got, optValue)
+			})
+		})
+	}
+}
+
+func erroneousArguments(t *testing.T, request *cmds.Request, set genericParameterArgumentPair) {
+	t.Helper()
+	var badValue = struct{}{}
+	for _, test := range []struct {
+		typeName  string
+		getMethod interface{}
+	}{
+		{
+			"string",
+			fscmds.GetStringArgument,
+		},
+		{
+			"bool",
+			fscmds.GetBoolArgument,
+		},
+		{
+			"Duration",
+			fscmds.GetDurationArgument,
+		},
+	} {
+		var (
+			typeName = test.typeName
+			getOpt   = test.getMethod
+		)
+		t.Run(typeName, func(t *testing.T) {
+			t.Run("Options", func(t *testing.T) {
+				got, provided := genericCheckArgument(t,
+					getOpt, set.arguments)
+				genericArgWasNotProvided(t,
+					typeName, provided, got)
+
+				request.SetOption(set.Name, badValue)
+				_, provided, err := genericGetArgument(getOpt, set.arguments)
+				if err == nil {
+					t.Fatalf("expected type error for bad request\n\ttype: %s\n\tvalue: %#v",
+						typeName, badValue)
+				}
+				delete(request.Options, set.Name)
+
+				genericArgWasProvided(t,
+					typeName, provided)
+			})
+			t.Run("Environment", func(t *testing.T) {
+				got, provided := genericCheckArgument(t,
+					getOpt, set.arguments)
+				genericArgWasNotProvided(t,
+					typeName, provided, got)
+
+				if typeName == "string" {
+					t.Skip("don't test string type against env var 0 value")
+				}
+
+				osErr := os.Setenv(set.Environment,
+					"Intentionally invalid test value for type: "+
+						typeName)
+				if osErr != nil {
+					t.Fatalf("failed to set environment %q: %s",
+						set.Environment, osErr)
+				}
+				_, provided, err := genericGetArgument(getOpt, set.arguments)
+				if osErr := os.Unsetenv(set.Environment); osErr != nil {
+					t.Fatalf("failed to unset environment %q: %s",
+						set.Environment, osErr)
+				}
+				if err == nil {
+					t.Fatalf("expected type error for bad request\n\ttype: %s\n\tvalue: %#v",
+						typeName, badValue)
+				}
+
+				genericArgWasProvided(t,
+					typeName, provided)
+			})
+		})
+	}
+}
+
+func genericCheckArgument(t *testing.T, method interface{},
+	goArguments []reflect.Value) (got reflect.Value, provided bool) {
+	t.Helper()
+	var err error
+	got, provided, err = genericGetArgument(method, goArguments)
 	if err != nil {
 		t.Fatal(err)
 	}
+	return
+}
 
-	t.Run("Expected", func(t *testing.T) {
-		var (
-			parameters = fscmds.CmdsParameterSet{
-				Name:        "test-argument",
-				Environment: "TEST_ENV",
-			}
-			goArguments = []reflect.Value{
-				reflect.ValueOf(request),
-				reflect.ValueOf(parameters),
-			}
+func genericArgIsSame(t *testing.T, typeName string, got reflect.Value, want interface{}) {
+	t.Helper()
+	if got.Interface().(interface{}) != want {
+		t.Logf("%T != %T\n", got, want)
+		t.Fatalf("%s option provided did not match input:\n\twant:%s\n\tgot:%s",
+			typeName,
+			want, got)
+	}
+}
 
-			// shorthand:
-			testParam = parameters.Name
-			testEnv   = parameters.Environment
-		)
-		for _, test := range []struct {
-			typeName  string
-			value     interface{}
-			getMethod interface{}
-		}{
-			{
-				"string",
-				"test string value",
-				fscmds.GetStringArgument,
-			},
-			{
-				"bool",
-				true,
-				fscmds.GetBoolArgument,
-			},
-		} {
-			var (
-				typeName = test.typeName
-				getOpt   = test.getMethod
-				optValue = test.value
-			)
-			t.Run(typeName, func(t *testing.T) {
-				t.Run("Options", func(t *testing.T) {
-					got, provided, err := callMethod(getOpt, goArguments)
-					if err != nil {
-						t.Fatal(err)
-					}
-					checkNotProvided(t, typeName, provided, got)
+func genericArgWasProvided(t *testing.T, typeName string, provided bool) {
+	t.Helper()
+	if !provided {
+		t.Fatalf("%s option was provided but not detected in request",
+			typeName)
+	}
+}
 
-					request.SetOption(testParam, optValue)
-					got, provided, err = callMethod(getOpt, goArguments)
-					delete(request.Options, testParam)
-					if err != nil {
-						t.Fatal(err)
-					}
-					checkProvided(t, typeName, provided)
-					checkSame(t, typeName, got, optValue)
-				})
-				t.Run("Environment", func(t *testing.T) {
-					got, provided, err := callMethod(getOpt, goArguments)
-					if err != nil {
-						t.Fatal(err)
-					}
-					checkNotProvided(t, typeName, provided, got)
-
-					osErr := os.Setenv(testEnv, fmt.Sprintf("%v", optValue))
-					if osErr != nil {
-						t.Fatalf("failed to set environment %q: %s", testEnv, osErr)
-					}
-					got, provided, err = callMethod(getOpt, goArguments)
-					if osErr := os.Unsetenv(testEnv); osErr != nil {
-						t.Fatalf("failed to unset environment %q: %s", testEnv, osErr)
-					}
-					if err != nil {
-						t.Fatal(err)
-					}
-
-					checkProvided(t, typeName, provided)
-					checkSame(t, typeName, got, optValue)
-				})
-			})
-		}
-	})
-
-	t.Run("Unexpected", func(t *testing.T) {
-		var (
-			badParameters = fscmds.CmdsParameterSet{
-				Name:        "bad-argument",
-				Environment: "BAD_ENV",
-			}
-			badValue    = struct{}{}
-			goArguments = []reflect.Value{
-				reflect.ValueOf(request),
-				reflect.ValueOf(badParameters),
-			}
-		)
-		for _, test := range []struct {
-			typeName  string
-			getMethod interface{}
-		}{
-			{
-				"string",
-				fscmds.GetStringArgument,
-			},
-			{
-				"bool",
-				fscmds.GetBoolArgument,
-			},
-			{
-				"Duration",
-				fscmds.GetDurationArgument,
-			},
-		} {
-			var (
-				typeName = test.typeName
-				getOpt   = test.getMethod
-
-				// shorthand:
-				badParameter = badParameters.Name
-				badEnv       = badParameters.Environment
-			)
-			t.Run(typeName, func(t *testing.T) {
-				t.Run("Options", func(t *testing.T) {
-					got, provided, err := callMethod(getOpt, goArguments)
-					if err != nil {
-						t.Fatal(err)
-					}
-					checkNotProvided(t, typeName, provided, got)
-
-					request.SetOption(badParameter, badValue)
-					_, provided, err = callMethod(getOpt, goArguments)
-					if err == nil {
-						t.Fatalf("expected type error for bad request\n\ttype: %s\n\tvalue: %#v",
-							typeName, badValue)
-					}
-					delete(request.Options, badParameter)
-					checkProvided(t, typeName, provided)
-				})
-				t.Run("Environment", func(t *testing.T) {
-					got, provided, err := callMethod(getOpt, goArguments)
-					if err != nil {
-						t.Fatal(err)
-					}
-					checkNotProvided(t, typeName, provided, got)
-
-					if typeName == "string" {
-						return // skip testing env type for strings
-					}
-
-					osErr := os.Setenv(badEnv, "Intentionally invalid test value for type: "+
-						typeName)
-					if osErr != nil {
-						t.Fatalf("failed to set environment %q: %s", badEnv, osErr)
-					}
-					_, provided, err = callMethod(getOpt, goArguments)
-					if osErr := os.Unsetenv(badEnv); osErr != nil {
-						t.Fatalf("failed to unset environment %q: %s", badEnv, osErr)
-					}
-					if err == nil {
-						t.Fatalf("expected type error for bad request\n\ttype: %s\n\tvalue: %#v",
-							typeName, badValue)
-					}
-					checkProvided(t, typeName, provided)
-				})
-			})
-		}
-	})
+func genericArgWasNotProvided(t *testing.T, typeName string, provided bool, got reflect.Value) {
+	t.Helper()
+	if provided {
+		t.Fatalf("%s returned for bad request:\n\tProvided? %t\n\tValue: %#v",
+			typeName, provided, got)
+	}
 }
 
 func TestRequestMultiaddr(t *testing.T) {
