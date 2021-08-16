0628a80c17ddf21c22322cefd8935732841ca904
Dominic Della Valle <ddvpublic@gmail.com>
2021-08-16 10:02:08 -0400

    check in the gross tests - these will be rewritten

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -3,28 +3,32 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
-	"io"
-	"io/fs"
 	"log"
+	"testing/fstest"
 	"time"
 
 	"github.com/djdv/go-filesystem-utils/filesystem/pinfs"
 	httpapi "github.com/ipfs/go-ipfs-http-client"
 	coreiface "github.com/ipfs/interface-go-ipfs-core"
-	coreoptions "github.com/ipfs/interface-go-ipfs-core/options"
 	"github.com/multiformats/go-multiaddr"
 	madns "github.com/multiformats/go-multiaddr-dns"
 )
 
+// TODO: we need to turn parts of this into proper Go tests.
+// (grab the daemon spawning + pin setup tests from the old module branch)
+
 func main() {
 	log.SetFlags(log.Lshortfile)
 
-	// TODO [report this][t-notes]: When this maddr is not correct, CoreAPI requests return
+	// TODO [report this][t-notes]: When this maddr is not correct
+	// (does not point to a dialable API endpoint), CoreAPI requests return
 	//`command not found`
-	// this is confusing and should say something at least when the connection isn't dialable.
-	// actual API compliance can be done by clients if needed,
-	// but the connection error should come back to us in this case
+	// This is confusing and should give some kind of useful error message
+	// at least when the connection isn't dialable.
+	// If it is dialable, API compliance could probably be left up to clients if needed,
+	// but connection errors should come back to us when trying to invoke a request.
+	// I.e. "could not connect to xyz" makes much more sense than "command not found".
+	// And "xyz is not an API server" would be ideal when it is dialable, but the wrong address.
 	//ipfsMaddr := multiaddr.StringCast("/ip4/127.0.0.1/tcp/8080")
 	ipfsMaddr := multiaddr.StringCast("/ip4/127.0.0.1/tcp/5001")
 
@@ -58,37 +62,44 @@ func main() {
 	//fsi := ipfs.NewInterface(context.Background(), coreAPI, filesystem.IPFS)
 	fsi := pinfs.NewInterface(ctx, coreAPI)
 
-	pinChan, err := coreAPI.Pin().Ls(ctx, coreoptions.Pin.Ls.Recursive())
-	if err != nil {
+	/*
+		pinChan, err := coreAPI.Pin().Ls(ctx, coreoptions.Pin.Ls.Recursive())
+		if err != nil {
+			log.Fatal(err)
+		}
+		for pin := range pinChan {
+			fmt.Println("pin from API:", pin.Path())
+		}
+
+		entries, err := fs.ReadDir(fsi, ".")
+		if !errors.Is(err, io.EOF) {
+			log.Fatal(err)
+		}
+
+		for _, ent := range entries {
+			fmt.Println("ent from FS:", ent.Name())
+		}
+
+		const aboutName = "QmPZ9gcCEpqKTo6aq61g2nXGUhM4iCL3ewB6LDXZCtioEB"
+
+		aboutFile, err := fsi.Open(aboutName)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		const aboutSize = 1681
+		aboutBuffer := make([]byte, aboutSize)
+		read, err := aboutFile.Read(aboutBuffer)
+		log.Printf("read: %d/%d, err: %v\n", read, aboutSize, err)
+		log.Printf("%s\n", aboutBuffer)
+
+		log.Println("close:", aboutFile.Close())
+	*/
+
+	if err := fstest.TestFS(fsi, "QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc"); err != nil {
+		//if err := fstest.TestFS(fsi); err != nil {
 		log.Fatal(err)
 	}
-	for pin := range pinChan {
-		fmt.Println("pin from API:", pin.Path())
-	}
-
-	entries, err := fs.ReadDir(fsi, ".")
-	if !errors.Is(err, io.EOF) {
-		log.Fatal(err)
-	}
-
-	for _, ent := range entries {
-		fmt.Println("ent from FS:", ent.Name())
-	}
-
-	const aboutName = "QmPZ9gcCEpqKTo6aq61g2nXGUhM4iCL3ewB6LDXZCtioEB"
-
-	aboutFile, err := fsi.Open(aboutName)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	const aboutSize = 1681
-	aboutBuffer := make([]byte, aboutSize)
-	read, err := aboutFile.Read(aboutBuffer)
-	log.Printf("read: %d/%d, err: %v\n", read, aboutSize, err)
-	log.Printf("%s\n", aboutBuffer)
-
-	log.Println("close:", aboutFile.Close())
 }
 
 func ipfsClient(apiMaddr multiaddr.Multiaddr) (coreiface.CoreAPI, error) {
--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/billziss-gh/cgofuse v1.5.0
 	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf
 	github.com/fatih/camelcase v1.0.0
+	github.com/ipfs/go-ipfs v0.9.1
+	github.com/ipfs/go-ipfs-chunker v0.0.5
 	github.com/ipfs/go-ipfs-cmds v0.6.0
 	github.com/ipfs/go-ipfs-config v0.16.0
 	github.com/ipfs/go-ipfs-files v0.0.8
@@ -15,10 +17,11 @@ require (
 	github.com/ipfs/go-ipld-format v0.2.0
 	github.com/ipfs/go-log v1.0.5
 	github.com/ipfs/go-merkledag v0.4.0
-	github.com/ipfs/go-unixfs v0.2.6
+	github.com/ipfs/go-mfs v0.1.2
+	github.com/ipfs/go-unixfs v0.2.5
 	github.com/ipfs/interface-go-ipfs-core v0.4.0
 	github.com/kardianos/service v0.0.0-00010101000000-000000000000
-	github.com/multiformats/go-multiaddr v0.3.1
+	github.com/multiformats/go-multiaddr v0.3.3
 	github.com/multiformats/go-multiaddr-dns v0.3.1
 	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015
 )
