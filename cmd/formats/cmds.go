723e7b4024c31d66b3f1f65fa7da970d6b0f0283
Dominic Della Valle <ddvpublic@gmail.com>
2021-09-03 12:35:01 -0400

    formats: add encoder map

--- cmd/formats/cmds.go
+++ cmd/formats/cmds.go
@@ -1,6 +1,7 @@
 package formats
 
 import (
+	"fmt"
 	"io"
 
 	cmds "github.com/ipfs/go-ipfs-cmds"
@@ -59,4 +60,59 @@ func MakeOptionalOutputs(request *cmds.Request, re cmds.ResponseEmitter) Optiona
 		outs.emit = re.Emit
 	}
 	return &outs
+}
+
+var (
+	CmdsEncoders = cmds.EncoderMap{
+		cmds.XML:  cmds.Encoders[cmds.XML],
+		cmds.JSON: cmds.Encoders[cmds.JSON],
+		cmds.Text: func(*cmds.Request) func(io.Writer) cmds.Encoder {
+			return func(w io.Writer) cmds.Encoder { return &textEncoder{w: w} }
+		},
+		cmds.TextNewline: func(*cmds.Request) func(io.Writer) cmds.Encoder {
+			return func(w io.Writer) cmds.Encoder {
+				return &textEncoder{w: w, terminateUnit: true}
+			}
+		},
+	}
+
+	errEmptyResponseRequest = fmt.Errorf("response's Request field must be populated")
+)
+
+type textEncoder struct {
+	w                         io.Writer
+	prefixUnit, terminateUnit bool
+}
+
+func (e *textEncoder) Encode(v interface{}) error {
+	if v == nil {
+		return errEmptyResponseRequest
+	}
+
+	const unitSeparator = '\x1F'
+	var prefix, postfix string
+	if e.prefixUnit {
+		prefix = string(unitSeparator)
+	}
+	if e.terminateUnit {
+		postfix = "\n"
+	}
+
+	_, encodingErr := fmt.Fprintf(e.w, "%s%v%s", prefix, v, postfix)
+	if encodingErr != nil {
+		return encodingErr
+	}
+
+	if !e.prefixUnit &&
+		!e.terminateUnit {
+		// If no formatting rules were explicitly provided;
+		// implicitly prefix subsequent values.
+		// Otherwise there's no way to know where 1 unit ends and another begins.
+		//
+		// While `\x1F` is not `\x20`, it usually renders as a space.
+		// So text should look like `unit1 unit2 unit3...` on a console,
+		// while still being machine parser friendly.
+		e.prefixUnit = true
+	}
+	return nil
 }
