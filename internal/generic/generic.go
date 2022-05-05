188c68f652f20960a50b834ec82f60e2d2e69ea6
Dominic Della Valle <ddvpublic@gmail.com>
2022-05-05 11:02:12 -0400

    Add generic helper package

---
+++ internal/generic/generic.go
@@ -0,0 +1,50 @@
+// Package generic provides a set of type agnostic helpers.
+package generic
+
+import "context"
+
+// Couple wraps 2 values (of any type)
+// under the generic names `Left` and `Right`.
+type Couple[left, right any] struct {
+	Left  left
+	Right right
+}
+
+// CtxEither receives from both channels,
+// relaying either the left or right type,
+// until both channels are closed or the context is done.
+func CtxEither[left, right any](ctx context.Context,
+	leftIn <-chan left, rightIn <-chan right,
+) <-chan Couple[left, right] {
+	eithers := make(chan Couple[left, right], cap(leftIn)+cap(rightIn))
+	go func() {
+		defer close(eithers)
+		for leftIn != nil ||
+			rightIn != nil {
+			var (
+				either Couple[left, right]
+				isOpen bool
+			)
+			select {
+			case either.Left, isOpen = <-leftIn:
+				if !isOpen {
+					leftIn = nil
+					continue
+				}
+			case either.Right, isOpen = <-rightIn:
+				if !isOpen {
+					rightIn = nil
+					continue
+				}
+			case <-ctx.Done():
+				return
+			}
+			select {
+			case eithers <- either:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return eithers
+}
---
+++ internal/generic/generic_test.go
@@ -0,0 +1,95 @@
+package generic_test
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/djdv/go-filesystem-utils/internal/generic"
+)
+
+func TestGeneric(t *testing.T) {
+	t.Parallel()
+	t.Run("CtxEither", ctxEither)
+}
+
+func ctxEither(t *testing.T) {
+	t.Parallel()
+	t.Run("valid", ctxEitherValid)
+	t.Run("invalid", ctxEitherInvalid)
+}
+
+func ctxEitherValid(t *testing.T) {
+	t.Parallel()
+	var (
+		wantLeft  = []int{1, 2}
+		wantRight = []string{"a", "b"}
+		gotLeft   = make([]int, 0, len(wantLeft))
+		gotRight  = make([]string, 0, len(wantRight))
+
+		ctx     = context.Background()
+		leftIn  = buffAndClose(wantLeft...)
+		rightIn = buffAndClose(wantRight...)
+	)
+	for leftOrRight := range generic.CtxEither(ctx, leftIn, rightIn) {
+		if left := leftOrRight.Left; left != 0 {
+			gotLeft = append(gotLeft, left)
+			continue
+		}
+		gotRight = append(gotRight, leftOrRight.Right)
+	}
+	var (
+		leftMatches  = reflect.DeepEqual(wantLeft, gotLeft)
+		rightMatches = reflect.DeepEqual(wantRight, gotRight)
+		mismatch     = !leftMatches || !rightMatches
+	)
+	if mismatch {
+		t.Errorf("Tuple did not match expected data"+
+			"\n\tgot: %#v %#v"+
+			"\n\twant: %#v %#v",
+			gotLeft, gotRight,
+			wantLeft, wantRight,
+		)
+	}
+}
+
+func ctxEitherInvalid(t *testing.T) {
+	t.Parallel()
+	t.Run("canceled", canceledEither)
+}
+
+func canceledEither(t *testing.T) {
+	t.Parallel()
+	t.Run("before send", canceledEitherBeforeSend)
+	t.Run("after send", canceledEitherAfterSend)
+}
+
+func canceledEitherBeforeSend(t *testing.T) {
+	t.Parallel()
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	// CtxEither starts canceled, and never receives any values from us.
+	// If this receive doesn't complete, the test will fail via time out.
+	<-generic.CtxEither(ctx, make(chan struct{}), (chan struct{})(nil))
+}
+
+func canceledEitherAfterSend(t *testing.T) {
+	t.Parallel()
+	var (
+		singleChan  = make(chan struct{})
+		ctx, cancel = context.WithCancel(context.Background())
+	)
+	go func() { singleChan <- struct{}{}; cancel() }()
+	// NOTE: Because of the runtime's randomness during `select`,
+	// the channel may send its value or be closed at this point.
+	<-generic.CtxEither(ctx, singleChan, (chan struct{})(nil))
+}
+
+func buffAndClose[in any](elements ...in) <-chan in {
+	out := make(chan in, len(elements))
+	for _, elem := range elements {
+		out <- elem
+	}
+	close(out)
+	return out
+}
