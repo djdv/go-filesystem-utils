96ba1d236c64b1ea8edf5d0106e58b23376dcad7
Dominic Della Valle <ddvpublic@gmail.com>
2022-05-05 11:02:12 -0400

    Add generic helper package

---
+++ internal/generic/generic.go
@@ -0,0 +1,154 @@
+/*
+Package generic provides a set of generic helper functions,
+primarily aimed at common channel operations.
+*/
+package generic
+
+import (
+	"context"
+	"sync"
+)
+
+func ctxRange[in any](ctx context.Context, input <-chan in) <-chan in {
+	relay := make(chan in, cap(input))
+	go func() {
+		defer close(relay)
+		for {
+			select {
+			case element, ok := <-input:
+				if !ok {
+					return
+				}
+				select {
+				case relay <- element:
+				case <-ctx.Done():
+					return
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return relay
+}
+
+func totalBuff[in any](inputs []<-chan in) (total int) {
+	for _, ch := range inputs {
+		total += cap(ch)
+	}
+	return
+}
+
+func CtxJoin[in any](ctx context.Context, inputs ...<-chan in) <-chan in {
+	out := make(chan in, totalBuff(inputs))
+	go func() {
+		defer close(out)
+		for _, source := range inputs {
+			for element := range ctxRange(ctx, source) {
+				select {
+				case out <- element:
+				case <-ctx.Done():
+					return
+				}
+			}
+		}
+	}()
+	return out
+}
+
+func CtxMerge[in any](ctx context.Context, sources ...<-chan in) <-chan in {
+	var (
+		mergedWg  sync.WaitGroup
+		mergedCh  = make(chan in, totalBuff(sources))
+		mergeFrom = func(ch <-chan in) {
+			defer mergedWg.Done()
+			for value := range ctxRange(ctx, ch) {
+				select {
+				case mergedCh <- value:
+				case <-ctx.Done():
+					return
+				}
+			}
+		}
+	)
+
+	mergedWg.Add(len(sources))
+	for _, source := range sources {
+		go mergeFrom(source)
+	}
+
+	go func() { mergedWg.Wait(); close(mergedCh) }()
+
+	return mergedCh
+}
+
+func CtxTakeAndCancel[in any](ctx context.Context, cancel context.CancelFunc,
+	inputs <-chan in, count int,
+) <-chan in {
+	relay := make(chan in, count)
+	go func() {
+		defer close(relay)
+		defer cancel()
+		for element := range ctxRange(ctx, inputs) {
+			if count == 0 {
+				return
+			}
+			select {
+			case relay <- element:
+			case <-ctx.Done():
+				return
+			}
+			count--
+		}
+	}()
+	return relay
+}
+
+func ForEachOrError[in any](ctx context.Context,
+	input <-chan in, errs <-chan error, phase func(in) error,
+) error {
+	for input != nil ||
+		errs != nil {
+		select {
+		case element, ok := <-input:
+			if !ok {
+				input = nil
+				continue
+			}
+			if err := phase(element); err != nil {
+				return err
+			}
+		case err, ok := <-errs:
+			if !ok {
+				errs = nil
+				continue
+			}
+			return err
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+	return nil
+}
+
+func ProcessResults[in, out any](ctx context.Context,
+	input <-chan in, output chan out, errors chan error,
+	phase func(in) (out, error),
+) {
+	for element := range ctxRange(ctx, input) {
+		result, err := phase(element)
+		if err != nil {
+			select {
+			case errors <- err:
+				continue
+			case <-ctx.Done():
+				return
+			}
+		}
+		select {
+		case output <- result:
+		case <-ctx.Done():
+			return
+		}
+	}
+}
