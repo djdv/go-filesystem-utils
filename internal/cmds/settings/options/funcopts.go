b8baa7845b9ba064a69e7c50b2183f5190b36e53
Dominic Della Valle <ddvpublic@gmail.com>
2022-05-22 02:38:18 -0400

    settings/options: consistent name verbosity

--- internal/cmds/settings/options/funcopts.go
+++ internal/cmds/settings/options/funcopts.go
@@ -8,8 +8,8 @@ type (
 		withBuiltin      bool
 	}
 
-	constructorOpt struct{ OptionConstructor }
-	builtinOpt     bool
+	constructorOption struct{ OptionConstructor }
+	builtinOption     bool
 )
 
 func parseConstructorOptions(options ...ConstructorOption) (settings constructorSettings) {
@@ -21,12 +21,12 @@ func parseConstructorOptions(options ...ConstructorOption) (settings constructor
 
 // WithBuiltin sets whether cmdslib native options
 // (such as `--help`, `--timeout`, and more) should be constructed.
-func WithBuiltin(b bool) ConstructorOption          { return builtinOpt(b) }
-func (b builtinOpt) apply(set *constructorSettings) { set.withBuiltin = bool(b) }
+func WithBuiltin(b bool) ConstructorOption             { return builtinOption(b) }
+func (b builtinOption) apply(set *constructorSettings) { set.withBuiltin = bool(b) }
 
 // WithMaker appends the OptionConstructor to an internal handler list.
-func WithMaker(maker OptionConstructor) ConstructorOption { return constructorOpt{maker} }
+func WithMaker(maker OptionConstructor) ConstructorOption { return constructorOption{maker} }
 
-func (constructor constructorOpt) apply(set *constructorSettings) {
+func (constructor constructorOption) apply(set *constructorSettings) {
 	set.userConstructors = append(set.userConstructors, constructor.OptionConstructor)
 }
