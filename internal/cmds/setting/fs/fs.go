da65a67f0a9b2d6858df88602b8bd52c86f60478
Dominic Della Valle <ddvpublic@gmail.com>
2022-06-06 13:51:36 -0400

    settings/fs: add missing Parse wrapper (WIP)

--- internal/cmds/setting/fs/fs.go
+++ internal/cmds/setting/fs/fs.go
@@ -4,8 +4,13 @@ package fs
 
 import (
 	"context"
+	"reflect"
+	"time"
 
+	"github.com/djdv/go-filesystem-utils/internal/cmds/setting/argument"
+	"github.com/djdv/go-filesystem-utils/internal/cmds/setting/environment"
 	"github.com/djdv/go-filesystem-utils/internal/cmds/setting/option"
+	"github.com/djdv/go-filesystem-utils/internal/cmds/setting/request"
 	"github.com/djdv/go-filesystem-utils/internal/cmds/setting/runtime"
 	"github.com/djdv/go-filesystem-utils/internal/parameter"
 	cmds "github.com/ipfs/go-ipfs-cmds"
@@ -35,4 +40,29 @@ func MustMakeOptions[setPtr runtime.SettingsType[settings],
 		panic(err)
 	}
 	return cmdsOpts
+}
+
+func Parse[setIntf runtime.SettingsType[settings], settings any](ctx context.Context,
+	req *cmds.Request,
+) (*settings, error) {
+	var (
+		typeHandlers = handlers()
+		sources      = []argument.SetFunc{
+			request.ValueSource(req),
+			environment.ValueSource,
+		}
+	)
+	return argument.Parse[setIntf](ctx, sources, typeHandlers...)
+}
+
+// TODO: Name.
+func handlers() []argument.TypeParser {
+	return []argument.TypeParser{
+		{
+			Type: reflect.TypeOf((*time.Duration)(nil)).Elem(),
+			ParseFunc: func(argument string) (interface{}, error) {
+				return time.ParseDuration(argument)
+			},
+		},
+	}
 }
