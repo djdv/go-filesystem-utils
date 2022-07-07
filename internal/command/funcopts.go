4a300432104600a94559e353f36e41a255691131
Dominic Della Valle <ddvpublic@gmail.com>
2022-07-07 10:56:16 -0400

    prototype: command library
    - Needs feedback + tests
    - The generic package is included but mostly unused. We can strip it
      later.

---
+++ internal/command/funcopts.go
@@ -0,0 +1,48 @@
+package command
+
+import "io"
+
+type (
+	Option func(*commandSettings) error
+
+	commandSettings struct {
+		niladic     bool
+		subcmds     []Command
+		usageOutput io.StringWriter
+	}
+)
+
+func WithSubcommands(subcommands ...Command) Option {
+	return func(settings *commandSettings) error {
+		settings.subcmds = subcommands
+		return nil
+	}
+}
+
+// TODO: docs; this is where the usage text gets printed
+// when args are not what execute() expects for the command.
+// Note that we're going to resolve nil to stderr
+// so make the user aware of this.
+func WithUsageOutput(output io.StringWriter) Option {
+	return func(settings *commandSettings) error {
+		settings.usageOutput = output
+		return nil
+	}
+}
+
+func WithoutArguments(niladic bool) Option {
+	return func(settings *commandSettings) error {
+		settings.niladic = niladic
+		return nil
+	}
+}
+
+func parseOptions(options ...Option) (*commandSettings, error) {
+	set := new(commandSettings)
+	for _, setFunc := range options {
+		if err := setFunc(set); err != nil {
+			return nil, err
+		}
+	}
+	return set, nil
+}
---
+++ internal/command/interface.go
@@ -0,0 +1,49 @@
+package command
+
+import (
+	"context"
+	"flag"
+
+	"github.com/djdv/go-filesystem-utils/internal/generic"
+)
+
+type (
+	Command interface {
+		// Name returns a human friendly name of the command.
+		// Which may be used to identify commands,
+		// as well as decorate user facing help-text.
+		Name() string
+
+		// Synopsis returns a single-line string describing the command.
+		Synopsis() string
+
+		// Usage returns an arbitrarily long string explaining how to use the command.
+		Usage() string
+
+		// Subcommands returns a list of subcommands (if any).
+		Subcommands() []Command
+
+		// Execute executes the command, with or without any arguments.
+		Execute(ctx context.Context, args ...string) error
+	}
+
+	// A FlagBinder should call the relevant `Var` methods of the FlagSet,
+	// with each of it's flag variable references.
+	// E.g. a struct would pass pointers to each of its fields,
+	// to `FlagSet.Var(&structABC.fieldXYZ, ...)`.
+	FlagBinder interface {
+		BindFlags(*flag.FlagSet)
+	}
+
+	// FlagSettings is a constraint that permits any reference type
+	// that can bind its value setter(s), to a [flag.FlagSet].
+	FlagSettings[settings any] interface {
+		*settings
+		FlagBinder
+	}
+)
+
+// ErrUsage may be returned from Execute if the provided arguments
+// do not match the expectations of the given command.
+// E.g. arguments in the wrong format/type, too few/many arguments, etc.
+const ErrUsage = generic.ConstError("command called with unexpected arguments")
