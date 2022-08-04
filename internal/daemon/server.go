60db751c6a567916bfbeae93b67fd8d644ac9605
Dominic Della Valle <ddvpublic@gmail.com>
2022-08-04 10:02:27 -0400

    daemon: change how sockets are managed and exposed

--- internal/daemon/server.go
+++ internal/daemon/server.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"strings"
 	"sync"
+	"sync/atomic"
 
 	"github.com/djdv/go-filesystem-utils/internal/files"
 	"github.com/hugelgupf/p9/p9"
@@ -20,15 +21,15 @@ import (
 
 type (
 	Server struct {
+		path   *atomic.Uint64
 		root   *files.Directory
 		server *p9.Server
 
 		closer io.Closer
 		log    ulog.Logger
 
-		servingWg sync.WaitGroup
-		uid       p9.UID
-		gid       p9.GID
+		uid p9.UID
+		gid p9.GID
 		// stopping  bool
 
 		// experimenting:
@@ -45,8 +46,9 @@ func (fn closer) Close() error { return fn() }
 
 func NewServer(options ...ServerOption) *Server {
 	server := &Server{
-		uid: p9.NoUID,
-		gid: p9.NoGID,
+		path: new(atomic.Uint64),
+		uid:  p9.NoUID,
+		gid:  p9.NoGID,
 	}
 	for _, setFunc := range options {
 		if err := setFunc(server); err != nil {
@@ -61,6 +63,24 @@ func (srv *Server) ListenAndServe(ctx context.Context, maddr multiaddr.Multiaddr
 	if err != nil {
 		return err
 	}
+
+	// TODO: lint; for testing  multiple listener logic
+	// Needs to be wired up to CLI somehow, and/or converted for test pkg.
+	// something like `daemon addServer existingDaemonSock newDaemonMaddr`
+	// (the likelyhood this will be needed is slim)
+	/*
+		go func() {
+				const maddrString = "/ip4/127.0.0.1/tcp/564"
+				listener, _ := manet.Listen(multiaddr.StringCast(maddrString))
+				srv.Serve(ctx, listener)
+			}()
+			go func() {
+				const maddrString = "/ip4/127.0.0.1/tcp/565"
+				listener, _ := manet.Listen(multiaddr.StringCast(maddrString))
+				srv.Serve(ctx, listener)
+			}()
+	*/
+
 	return srv.Serve(ctx, listener)
 }
 
@@ -73,16 +93,14 @@ func (srv *Server) Serve(ctx context.Context, listener manet.Listener) error {
 	var (
 		root   = srv.root
 		server = srv.server
+		path   = srv.path
 	)
 	if root == nil {
-		const (
-			owner       = 6
-			group       = 3
-			permissions = p9.Exec<<owner | p9.Write<<owner | p9.Read<<owner |
-				p9.Exec<<group | p9.Read<<group |
-				p9.Exec | p9.Read
-		)
+		const permissions = S_IRWXU |
+			S_IRGRP | S_IXGRP |
+			S_IRUSR | S_IXUSR
 		root = files.NewDirectory(
+			files.WithPath[files.DirectoryOption](path),
 			files.WithPermissions[files.DirectoryOption](permissions),
 			files.WithUID[files.DirectoryOption](srv.uid),
 			files.WithGID[files.DirectoryOption](srv.gid),
@@ -95,25 +113,15 @@ func (srv *Server) Serve(ctx context.Context, listener manet.Listener) error {
 		srv.root = root
 		srv.server = server
 	}
-	var (
-		closed bool
 
-		sockName = strings.ReplaceAll(
-			listener.Addr().String(),
-			"/", "|",
-		)
-		listenerCloser closer = func() error {
-			closed = true
-			return listener.Close()
-		}
-	)
-	if err := addCloserFile(sockName, listenerCloser, root); err != nil {
+	closed, err := storeListener(listener, root, path, srv.uid, srv.gid)
+	if err != nil {
 		return err
 	}
 
 	var (
 		wg                sync.WaitGroup
-		conns, acceptErrs = accept(ctx, &closed, listener)
+		conns, acceptErrs = accept(ctx, closed, listener)
 		handlerErrs       = handle9(ctx, conns, server.Handle, &wg)
 
 		// TODO: gross
@@ -158,23 +166,166 @@ func (srv *Server) Serve(ctx context.Context, listener manet.Listener) error {
 	return retErr
 }
 
-// [45ecbfb2-430b-48e0-847d-a6f78eac7816]
-// TODO: root type should be p9.File
-// path should be constructed and passed to root, and shared with newCloser
-// not retrieved from getter method.
-func addCloserFile(name string, closer io.Closer, root *files.Directory) error {
-	listenerDir, err := getListenerDir(root)
+// TODO: docs
+// bool will be true when some arbitrary thread calls close.
+// We need this to distinguish between an intentional close
+// and unexpected socket failure.
+func storeListener(listener manet.Listener, root p9.File,
+	path *atomic.Uint64, uid p9.UID, gid p9.GID,
+) (*atomic.Bool, error) {
+	listenersDir, err := getListenerDir(root)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	closerFile, _ := files.NewCloser(closer,
-		files.WithParent[files.CloserOption](listenerDir),
-		files.WithPath[files.CloserOption](root.Path()),
-	)
-	if err := listenerDir.Link(closerFile, name); err != nil {
-		return err
-	}
+	return storeListenerFile(listener, listenersDir, path, uid, gid)
+}
 
+func storeListenerFile(listener manet.Listener, listenersDir p9.File,
+	path *atomic.Uint64, uid p9.UID, gid p9.GID,
+) (*atomic.Bool, error) {
+	var (
+		// TODO: [safety] The prefix trim should be safe by the spec
+		// I'm not sure if we have guarantees about component counts.
+		components = strings.Split(listener.Multiaddr().String(), "/")[1:]
+		tail       = len(components) - 1
+		dirs       = components[:tail]
+		file       = components[tail]
+	)
+	listenerDir, err := mkdirAll(listenersDir, dirs, uid, gid)
+	if err != nil {
+		return nil, err
+	}
+	var (
+		closerFile p9.File
+		closed     = new(atomic.Bool)
+		closeFunc  = closer(func() error {
+			closed.Store(true)
+			lErr := listener.Close()
+
+			// TODO: handle non-listeners errors
+			if err := closerFile.UnlinkAt(".", 0); err != nil {
+				log.Printf("DBG: unlink %s err: %s", file, err)
+			}
+			if err := removeEmpties(listenersDir, dirs); err != nil {
+				log.Printf("DBG: cleanup for %s - %s err: %s", dirs, file, err)
+			}
+
+			return lErr
+		})
+	)
+	closerFile = makeListenerFile(closeFunc, listenerDir, path, uid, gid)
+	if err := listenerDir.Link(closerFile, file); err != nil {
+		return nil, err
+	}
+	return closed, nil
+}
+
+func makeListenerFile(closer io.Closer, parent p9.File,
+	path *atomic.Uint64, uid p9.UID, gid p9.GID,
+) p9.File {
+	closerFile, _ := files.NewCloser(closer,
+		files.WithParent[files.CloserOption](parent),
+		files.WithPath[files.CloserOption](path),
+		files.WithUID[files.CloserOption](uid),
+		files.WithGID[files.CloserOption](gid),
+	)
+	return closerFile
+}
+
+func mkdirAll(dir p9.File, names []string, uid p9.UID, gid p9.GID) (p9.File, error) {
+	var (
+		closers  = make([]io.Closer, 0, len(names))
+		closeAll = func() error {
+			for _, c := range closers {
+				if err := c.Close(); err != nil {
+					return err
+				}
+			}
+			closers = nil
+			return nil
+		}
+	)
+	defer closeAll() // TODO: error needs to be caught and appended if we return early.
+
+	const (
+		// TODO: permission audit
+		permissions = S_IRWXU |
+			S_IXGRP | S_IRGRP |
+			S_IXOTH | S_IROTH
+	)
+	tail := len(names) - 1
+	for i, name := range names {
+		name9 := []string{name}
+		_, next, err := dir.Walk(name9)
+		if err != nil {
+			if !errors.Is(err, perrors.ENOENT) {
+				return nil, err
+			}
+			if _, err := dir.Mkdir(name, permissions, uid, gid); err != nil {
+				return nil, err
+			}
+			if _, next, err = dir.Walk(name9); err != nil {
+				return nil, err
+			}
+		}
+		if i != tail {
+			closers = append(closers, next)
+		}
+		dir = next
+	}
+	if err := closeAll(); err != nil {
+		return nil, err
+	}
+	return dir, nil
+}
+
+// XXX this whole thang is likely more nasty than it has to be.
+// If anything fails in here we're likely going to get zombie files that might ruin things.
+// Likely fine for empty directories, but not endpoints. That shouldn't happen though.
+// "shouldn't"
+// TODO: name needs to imply reverse order, or take an order param
+func removeEmpties(root p9.File, dirs []string) error {
+	var (
+		cur      = root
+		nwname   = len(dirs)
+		dirFiles = make([]p9.File, nwname)
+
+		// TODO: micro-opt; is this faster than allocating in the loop?
+		wname = make([]string, 1)
+	)
+	for i, name := range dirs {
+		wname[0] = name
+		_, dir, err := cur.Walk(wname)
+		if err != nil {
+			return err
+		}
+		cur = dir
+		dirFiles[i] = cur
+	}
+	for i := nwname - 1; i >= 0; i-- {
+		cur := dirFiles[i]
+		ents, err := files.ReadDir(cur)
+		if err != nil {
+			return err
+		}
+		if len(ents) == 0 {
+			// XXX: we're avoiding `Walk(..)` here
+			// but it's hacky and gross. Our indexing should be better,
+			// or we should just do the walk.
+			var (
+				parent p9.File
+				name   = dirs[i]
+			)
+			if i == 0 {
+				parent = root
+			} else {
+				parent = dirFiles[i-1]
+			}
+			if err := parent.UnlinkAt(name, 0); err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
 
@@ -208,13 +359,11 @@ func getListenerDir(root p9.File) (p9.File, error) {
 	}
 
 	const (
-		owner = 6
-		group = 3
 		// TODO: permission audit
 		// Do we want to allow non-owner to list these? exec may be fine, but not read.
-		permissions = p9.Exec<<owner | p9.Write<<owner | p9.Read<<owner |
-			p9.Exec<<group | p9.Read<<group |
-			p9.Exec | p9.Read
+		permissions = S_IRWXU |
+			S_IXGRP | S_IRGRP |
+			S_IXOTH | S_IROTH
 	)
 	if _, err := root.Mkdir(dirName, permissions, attr.UID, attr.GID); err != nil {
 		return nil, err
@@ -223,7 +372,7 @@ func getListenerDir(root p9.File) (p9.File, error) {
 	return listenerDir, err
 }
 
-func accept(ctx context.Context, closed *bool, listener manet.Listener) (<-chan manet.Conn, <-chan error) {
+func accept(ctx context.Context, closed *atomic.Bool, listener manet.Listener) (<-chan manet.Conn, <-chan error) {
 	var (
 		conns = make(chan manet.Conn)
 		errs  = make(chan error)
@@ -234,7 +383,7 @@ func accept(ctx context.Context, closed *bool, listener manet.Listener) (<-chan 
 		for {
 			conn, err := listener.Accept()
 			if err != nil {
-				if *closed && // Listener closed intentionally.
+				if closed.Load() && // Listener closed intentionally.
 					errors.Is(err, net.ErrClosed) {
 					return
 				}
