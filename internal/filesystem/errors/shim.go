6f72240bb366afb303d4957d63443a430e9cf326
Dominic Della Valle <ddvpublic@gmail.com>
2022-11-22 03:59:25 -0500

    fserrors: add Join shim for Go <1.20
    This should be temporary until 1.20 lands.
    All calls to `fserrors`.Join will revert
    to `errors.Join` when it's in the standard lib.

---
+++ internal/filesystem/errors/shim.go
@@ -0,0 +1,64 @@
+//go:build !go1.20
+
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package errors
+
+import "errors"
+
+// Join returns an error that wraps the given errors.
+// Any nil error values are discarded.
+// Join returns nil if errs contains no non-nil values.
+// The error formats as the concatenation of the strings obtained
+// by calling the Error method of each element of errs, with a newline
+// between each string.
+func Join(errs ...error) error {
+	n := 0
+	for _, err := range errs {
+		if err != nil {
+			n++
+		}
+	}
+	if n == 0 {
+		return nil
+	}
+	e := &joinError{
+		errs: make([]error, 0, n),
+	}
+	for _, err := range errs {
+		if err != nil {
+			e.errs = append(e.errs, err)
+		}
+	}
+	return e
+}
+
+type joinError struct {
+	errs []error
+}
+
+func (e *joinError) Error() string {
+	var b []byte
+	for i, err := range e.errs {
+		if i > 0 {
+			b = append(b, '\n')
+		}
+		b = append(b, err.Error()...)
+	}
+	return string(b)
+}
+
+func (e *joinError) Unwrap() []error {
+	return e.errs
+}
+
+func (e *joinError) Is(target error) bool {
+	for _, err := range e.Unwrap() {
+		if errors.Is(err, target) {
+			return true
+		}
+	}
+	return false
+}
---
+++ internal/filesystem/errors/shim_20.go
@@ -0,0 +1,7 @@
+//go:build go1.20
+
+package errors
+
+import "errors"
+
+func Join(errs ...error) error { return errors.Join(errs...) }
