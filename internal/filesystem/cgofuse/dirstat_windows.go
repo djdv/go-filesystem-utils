d0277d7417d6ee71fae9b20e4113596796ca74f2
Dominic Della Valle <ddvpublic@gmail.com>
2022-12-17 16:47:25 -0500

    cgofuse: directory operations refactor
    Directories are reworked to better comply with `fs.FS` expectations,
    and utilize streaming extensions of it.
    
    Some modification operations (like `Rmdir`) gained nascent
    implementations.
    
    Platform specific behavior is contained within build constrained files.

---
+++ internal/filesystem/cgofuse/dirstat_windows.go
@@ -0,0 +1,19 @@
+//go:build !nofuse
+
+package cgofuse
+
+import (
+	"io/fs"
+
+	"github.com/winfsp/cgofuse/fuse"
+)
+
+func dirStat(ent fs.DirEntry, fCtx fuseContext) (*fuse.Stat_t, error) {
+	info, err := ent.Info()
+	if err != nil {
+		return nil, err
+	}
+	stat := new(fuse.Stat_t)
+	goToFuseStat(info, fCtx, stat)
+	return stat, nil
+}
