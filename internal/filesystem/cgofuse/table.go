5d5ee085486f3b8f05b65ecbaaced95ee7e3d03a
Dominic Della Valle <ddvpublic@gmail.com>
2022-12-17 16:13:52 -0500

    cgofuse: refactor file table
    The map version was only for prototyping. The implementation was bad
    since it's memory would grow indefinitely.
    This slice version will dynamically allocate and free memory as needed.
    
    The hard-coded limits and factors are arbitrary and should become
    variable at some point. Likely options within the constructor.

--- internal/filesystem/cgofuse/table.go
+++ internal/filesystem/cgofuse/table.go
@@ -1,5 +1,4 @@
 //go:build !nofuse
-// +build !nofuse
 
 package cgofuse
 
@@ -7,134 +6,207 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
+	"math"
 	"sync"
-)
 
-// TODO: review whole file; quickly ported
+	fserrors "github.com/djdv/go-filesystem-utils/internal/filesystem/errors"
+	"github.com/djdv/go-filesystem-utils/internal/generic"
+	"github.com/winfsp/cgofuse/fuse"
+)
 
 type (
-	handle     = uint64
-	fileHandle struct {
-		goFile fs.File
-		ioMu   sync.Mutex // TODO: name and responsibility; currently applies to the position cursor
+	handleSlice []*fileHandle
+	fileTable   struct {
+		sync.RWMutex
+		files handleSlice
 	}
-	fMap map[handle]*fileHandle
 )
 
-/*
-type (
-	handle       = uint64
-	fileResource struct {
-		sync.Mutex
-		fs.File
-	}
-	fMap map[handle]fileResource
-
-	directoryResource struct {
-		sync.Mutex
-		// TODO: copy the note about implementation differences here
-		// Fuse only implies this context has to be valid for open and creation operations
-		// and thus some implementations return zero or garbage values
-		// during operations like readdir (where we might need them depending on the platform)
-		fuseContext struct {
-			uid, gid uint32
-			// pid omitted as it's currently unused
-		}
-		fs.ReadDirFile
-	}
-	dMap map[handle]directoryResource
+const (
+	errorHandle = math.MaxUint64
+	// TODO: handleMax needs to be configurable like `ulimit` allows.
+	// NOTE: file table sizes and bounds were chosen arbitrarily.
+	// Suggestions for better averages or ways to tune are welcome.
+	handleMax              = 4096 // TODO: POSIX calls this "OPEN_MAX", we could in camelCase.
+	tableStartingSize      = 8
+	tableGrowthfactor      = 2
+	tableShrinkLimitFactor = tableGrowthfactor * 2
+	tableShrinkBound       = tableStartingSize * tableShrinkLimitFactor
 )
-*/
 
 var (
 	errInvalidHandle = errors.New("handle not found")
 	errFull          = errors.New("all slots filled")
 )
 
-func newFileTable() fileTable { return &fileTableStruct{files: make(fMap)} }
+func newFileTable() *fileTable { return new(fileTable) }
 
-type (
-	fileTable interface {
-		Add(fs.File) (handle, error)
-		Exists(handle) bool
-		Get(handle) (*fileHandle, error)
-		Remove(handle) error
-		Length() int
-		Close() error
-	}
-	fileTableStruct struct {
-		sync.RWMutex
-		index   uint64
-		wrapped bool // if true; we start reclaiming dead index values
-		files   fMap
-	}
-)
-
-func (ft *fileTableStruct) Add(f fs.File) (handle, error) {
-	ft.Lock()
-	defer ft.Unlock()
-
-	ft.index++
-	if !ft.wrapped && ft.index == handleMax {
-		ft.wrapped = true
-	}
-
-	if ft.wrapped { // switch from increment mode to "search for free slot" mode
-		for index := handle(0); index != handleMax; index++ {
-			if _, ok := ft.files[index]; ok {
-				// handle is in use
-				continue
-			}
-			// a free handle was found; use it
-			ft.index = index
-			return index, nil
+func (files handleSlice) lowestAvailable() fileDescriptor {
+	for i, handle := range files {
+		if handle == nil {
+			return fileDescriptor(i)
 		}
-		return errorHandle, errFull
 	}
-
-	// we've never hit the cap, so we can assume the handle is free
-	// but for sanity we check anyway
-	if _, ok := ft.files[ft.index]; ok {
-		panic("handle should be uninitialized but is in use")
-	}
-	ft.files[ft.index] = &fileHandle{goFile: f}
-	return ft.index, nil
+	return errorHandle
 }
 
-func (ft *fileTableStruct) Exists(fh handle) bool {
-	ft.RLock()
-	defer ft.RUnlock()
-	_, exists := ft.files[fh]
-	return exists
-}
-
-func (ft *fileTableStruct) Get(fh handle) (*fileHandle, error) {
-	ft.RLock()
-	defer ft.RUnlock()
-	f, exists := ft.files[fh]
-	if !exists {
-		return nil, errInvalidHandle
+func (files handleSlice) extend() (handleSlice, error) {
+	var (
+		filesLen = len(files)
+		filesEnd = filesLen + 1
+		filesCap = cap(files)
+	)
+	if filesLen < filesCap {
+		return files[:filesEnd], nil
 	}
-	return f, nil
+	var (
+		scaledCap = filesCap * tableGrowthfactor
+		newCap    = generic.Max(scaledCap, tableStartingSize)
+	)
+	if newCap > handleMax {
+		return nil, errFull
+	}
+	newTable := make([]*fileHandle, filesEnd, newCap)
+	copy(newTable, files)
+	return newTable, nil
 }
 
-func (ft *fileTableStruct) Remove(fh handle) error {
+func (files handleSlice) shrink(lowerBound int) handleSlice {
+	var (
+		emptySlots int
+		filesLen   = len(files)
+	)
+	for i := filesLen - 1; i >= 0; i-- {
+		if files[i] != nil {
+			break
+		}
+		emptySlots++
+	}
+	var (
+		newLen             = filesLen - emptySlots
+		newCap             = lowestAlignment(newLen, tableStartingSize)
+		tooSmall           = newCap < lowerBound
+		sameSize           = newCap == cap(files)
+		lessOrEqualToBound = tooSmall || sameSize
+	)
+	if lessOrEqualToBound {
+		return nil
+	}
+	newTable := make(handleSlice, newLen, newCap)
+	copy(newTable, files)
+	return newTable
+}
+
+func lowestAlignment(size, alignment int) int {
+	remainder := (size - 1) % alignment
+	return (size - 1) + (alignment - remainder)
+}
+
+func (ft *fileTable) add(f fs.File) (fileDescriptor, error) {
 	ft.Lock()
 	defer ft.Unlock()
-	if _, exists := ft.files[fh]; !exists {
+	files := ft.files
+	if index := files.lowestAvailable(); index != errorHandle {
+		files[index] = &fileHandle{goFile: f}
+		return index, nil
+	}
+	newTable, err := files.extend()
+	if err != nil {
+		return errorHandle, err
+	}
+
+	index := len(newTable) - 1
+	newTable[index] = &fileHandle{goFile: f}
+	ft.files = newTable
+	return fileDescriptor(index), nil
+}
+
+func (ft *fileTable) validLocked(fh fileDescriptor) error {
+	var (
+		files           = ft.files
+		descriptorCount = len(files)
+	)
+	if descriptorCount == 0 {
 		return errInvalidHandle
 	}
-	delete(ft.files, fh)
+	highestDescriptor := fileDescriptor(descriptorCount - 1)
+	if highestDescriptor < fh {
+		return errInvalidHandle
+	}
+	if files[fh] == nil {
+		return errInvalidHandle
+	}
 	return nil
 }
 
-func (ft *fileTableStruct) Length() int {
+func (ft *fileTable) get(fh fileDescriptor) (*fileHandle, error) {
 	ft.RLock()
 	defer ft.RUnlock()
-	return len(ft.files)
+	if err := ft.validLocked(fh); err != nil {
+		return nil, err
+	}
+	return ft.files[fh], nil
 }
 
-func (ft *fileTableStruct) Close() error {
+func (ft *fileTable) remove(fh fileDescriptor) error {
+	ft.Lock()
+	defer ft.Unlock()
+	if err := ft.validLocked(fh); err != nil {
+		return err
+	}
+	files := ft.files
+	files[fh] = nil
+	if cap(files) > tableShrinkBound {
+		if newTable := files.shrink(tableShrinkBound); newTable != nil {
+			ft.files = newTable
+		}
+	}
+	return nil
+}
+
+// TODO: [review] [Ame].
+// This function must be scrutinized heavily and should be tested well.
+// Currently neither are done.
+// Closing the file,
+// removing it's handle from the table,
+// and preserving all errors (both POSIX's and Go's)
+// are a must. Otherwise we'll retain dead references in the table,
+// and neither the operator nor operating system will know what happened.
+// (^Very bad if this happens.)
+func (ft *fileTable) release(fh fileDescriptor) (errorCode errNo, err error) {
+	file, err := ft.get(fh)
+	if err != nil {
+		return -fuse.EBADF, err
+	}
+	// NOTE: SUSv4;BSi7 `close` (paraphrased)
+	// "If errors are encountered, the result of the handle is unspecified."
+	// We'll remove the handle regardless of what the file's [Close] method does.
+	defer func() {
+		// User's [Close] method could panic.
+		if r := recover(); r != nil {
+			err = fmt.Errorf("panic: %v", r)
+			errorCode = -fuse.EIO
+		}
+		if rErr := ft.remove(fh); rErr != nil {
+			// We already checked the validity of fh
+			// and passed in [get]. So if this branch is hit,
+			// it implies the file table implementation is broken
+			// or memory was corrupted somehow.
+			//
+			// Don't overwrite error code if one was encountered.
+			if errorCode == operationSuccess {
+				errorCode = -fuse.EBADF
+			}
+			err = fserrors.Join(err, rErr)
+		}
+	}()
+	if err := file.goFile.Close(); err != nil {
+		return -fuse.EIO, err
+	}
+	return operationSuccess, nil
+}
+
+func (ft *fileTable) Close() error {
 	ft.Lock()
 	defer ft.Unlock()
 	var err error
