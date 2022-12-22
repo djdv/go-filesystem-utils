57edbe81ee172857e8d361c718428d025039e465
Dominic Della Valle <ddvpublic@gmail.com>
2022-12-22 06:40:12 -0500

    files: adapt to FUSE API changes

--- internal/files/fuse.go
+++ internal/files/fuse.go
@@ -8,6 +8,7 @@ import (
 	"github.com/djdv/go-filesystem-utils/internal/filesystem"
 	"github.com/djdv/go-filesystem-utils/internal/filesystem/cgofuse"
 	"github.com/hugelgupf/p9/p9"
+	"github.com/u-root/uio/ulog"
 )
 
 type (
@@ -79,4 +80,28 @@ func (dir *FuseDir) Mkdir(name string, permissions p9.FileMode, _ p9.UID, gid p9
 	return qid, dir.Link(fsiDir, name)
 }
 
-func mountFuse(fsys fs.FS, target string) (io.Closer, error) { return cgofuse.MountFuse(fsys, target) }
+type closer func() error // TODO: move/obviate this
+
+func (close closer) Close() error { return close() }
+
+func mountFuse(fsys fs.FS, target string) (io.Closer, error) {
+	// TODO: conditionally store and share this mounter
+	// right now we construct one every time.
+	// return cgofuse.MountFuse(fsys, target)
+
+	// TODO: pipe these properly; for debugging right now
+	opts := []cgofuse.WrapperOption{
+		cgofuse.WithLog(ulog.Log),
+	}
+	//
+	mounter, err := cgofuse.FSToFuse(fsys, opts...)
+	if err != nil {
+		return nil, err
+	}
+	if err := mounter.Mount(target); err != nil {
+		return nil, err
+	}
+	return closer(func() error {
+		return mounter.Unmount(target)
+	}), nil
+}
