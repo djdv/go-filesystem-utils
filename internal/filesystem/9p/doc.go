9eda3707288c7fefe3d1936541af55699676cb6b
Dominic Della Valle <ddvpublic@gmail.com>
2023-05-20 05:02:26 -0400

    9p: lint - move const out of doc.go

--- internal/filesystem/9p/doc.go
+++ internal/filesystem/9p/doc.go
@@ -1,11 +1,3 @@
 // Package p9 implements file systems for the
 // Plan 9 File Protocol.
 package p9
-
-import "github.com/hugelgupf/p9/perrors"
-
-// NOTE: [2023.01.02]
-// The reference documentation and implementation
-// do not specify which error number to use.
-// If this value seems incorrect, request to change it.
-const fidOpenedErr = perrors.EBUSY
