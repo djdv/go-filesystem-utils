023c0867414ff1b464966600dd815e5cb66fbb82
Dominic Della Valle <ddvpublic@gmail.com>
2023-05-21 03:20:04 -0400

    9p: mount - address type concern regarding file creation
    Previously, the mounts directory had a rigid format that had to be
    conformed to. This is no longer the case. Our implementation still works
    as a 3 level directory tree, but alternative versions could technically
    be implemented with regular files, or whatever other scheme. Using our
    specific directory types, their own types, or a combination.

--- internal/filesystem/9p/host.go
+++ internal/filesystem/9p/host.go
@@ -18,8 +18,9 @@ type (
 	HosterOption func(*hosterSettings) error
 	// MakeGuestFunc should handle file creation operations
 	// for files representing a [filesystem.ID].
+	// The file `mode` will contain file type bits.
 	MakeGuestFunc func(parent p9.File, guest filesystem.ID,
-		permissions p9.FileMode,
+		mode p9.FileMode,
 		uid p9.UID, gid p9.GID) (p9.QID, p9.File, error)
 )
 
@@ -56,10 +57,33 @@ func (hd *HostFile) Mkdir(name string, permissions p9.FileMode, uid p9.UID, gid 
 	if err != nil {
 		return p9.QID{}, err
 	}
+	mode := permissions | p9.ModeDirectory
 	qid, file, err := hd.makeGuestFn(hd, filesystem.ID(name),
-		permissions, uid, gid)
+		mode, uid, gid)
 	if err != nil {
 		return p9.QID{}, fserrors.Join(perrors.EACCES, err)
 	}
 	return qid, hd.Link(file, name)
+}
+
+func (hf *HostFile) Create(name string, flags p9.OpenFlags, permissions p9.FileMode,
+	uid p9.UID, gid p9.GID,
+) (p9.File, p9.QID, uint32, error) {
+	return createViaMknod(hf, name, flags, permissions, uid, gid)
+}
+
+func (hf *HostFile) Mknod(name string, mode p9.FileMode,
+	_, _ uint32, uid p9.UID, gid p9.GID,
+) (p9.QID, error) {
+	uid, gid, err := mkPreamble(hf, name, uid, gid)
+	if err != nil {
+		return p9.QID{}, err
+	}
+	mode |= p9.ModeRegular
+	qid, file, err := hf.makeGuestFn(hf, filesystem.ID(name),
+		mode, uid, gid)
+	if err != nil {
+		return p9.QID{}, fserrors.Join(perrors.EACCES, err)
+	}
+	return qid, hf.Link(file, name)
 }
