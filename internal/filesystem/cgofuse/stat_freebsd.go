605c3e5b26a56b8b6e544e50d35c90553201f819
Dominic Della Valle <ddvpublic@gmail.com>
2023-07-12 18:49:39 -0400

    cgofuse: add Statfs support (WIP)

---
+++ internal/filesystem/cgofuse/stat_freebsd.go
@@ -0,0 +1,35 @@
+package cgofuse
+
+import (
+	"syscall"
+
+	"github.com/winfsp/cgofuse/fuse"
+	"golang.org/x/sys/unix"
+)
+
+func statfs(path string, fStatfs *fuse.Statfs_t) (int, error) {
+	// HACK: see note in stat_windows.go
+	path = "/"
+	sysStat := &unix.Statfs_t{}
+	if err := unix.Statfs(path, sysStat); err != nil {
+		if errno, ok := err.(syscall.Errno); ok {
+			return int(errno), err
+		}
+		return -fuse.EACCES, err
+	}
+
+	// NOTE: These values are ignored by cgofuse
+	// but fsid might be incorrect on some platforms too
+	fStatfs.Fsid = uint64(sysStat.Fsid.Val[0])<<32 | uint64(sysStat.Fsid.Val[1])
+	fStatfs.Flag = uint64(sysStat.Flags)
+
+	fStatfs.Bsize = sysStat.Bsize
+	fStatfs.Blocks = sysStat.Blocks
+	fStatfs.Bfree = sysStat.Bfree
+	fStatfs.Bavail = uint64(sysStat.Bavail)
+	fStatfs.Files = sysStat.Files
+	fStatfs.Ffree = uint64(sysStat.Ffree)
+	fStatfs.Frsize = uint64(sysStat.Bsize)
+	fStatfs.Namemax = uint64(sysStat.Namemax)
+	return operationSuccess, nil
+}
---
+++ internal/filesystem/cgofuse/stat_openbsd.go
@@ -0,0 +1,35 @@
+package cgofuse
+
+import (
+	"syscall"
+
+	"github.com/winfsp/cgofuse/fuse"
+	"golang.org/x/sys/unix"
+)
+
+func statfs(path string, fStatfs *fuse.Statfs_t) (int, error) {
+	// HACK: see note in stat_windows.go
+	path = "/"
+	sysStat := &unix.Statfs_t{}
+	if err := unix.Statfs(path, sysStat); err != nil {
+		if errno, ok := err.(syscall.Errno); ok {
+			return int(errno), err
+		}
+		return -fuse.EACCES, err
+	}
+
+	// NOTE: These values are ignored by cgofuse
+	// but fsid might be incorrect on some platforms too
+	fStatfs.Fsid = uint64(sysStat.F_fsid.Val[0])<<32 | uint64(sysStat.F_fsid.Val[1])
+	fStatfs.Flag = uint64(sysStat.F_flags)
+
+	fStatfs.Bsize = uint64(sysStat.F_bsize)
+	fStatfs.Blocks = sysStat.F_blocks
+	fStatfs.Bfree = sysStat.F_bfree
+	fStatfs.Bavail = uint64(sysStat.F_bavail)
+	fStatfs.Files = sysStat.F_files
+	fStatfs.Ffree = uint64(sysStat.F_ffree)
+	fStatfs.Frsize = uint64(sysStat.F_bsize)
+	fStatfs.Namemax = uint64(sysStat.F_namemax)
+	return operationSuccess, nil
+}
